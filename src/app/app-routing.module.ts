2e69ca6d246ba8bc52d28d4a1934cdee00002b4b
AwaisUr <90728884+AwaisUr@users.noreply.github.com>
2021-09-15 16:16:16 +0300

    Add files via upload

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,31 +1,17 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
 import { EmployeeSearchComponent } from './unishf/employee-search/employee-search.component';
 import { NotFoundComponent } from './unishf/not-found/not-found.component';
 
 const routes: Routes = [
-
-  {  path:'',
-      component : EmployeeSearchComponent
-    },
-      {  path:'heroes',
-      component : EmployeeSearchComponent
-    },
-    {  path:'heroes?',
-      component : EmployeeSearchComponent
-    },
-    {  path:'**',
-    component : NotFoundComponent
-  }
-    
- 
-
-
-
+  { path: '', component: EmployeeSearchComponent },
+  { path: 'heroes', component: EmployeeSearchComponent },
+  { path: 'heroes?', component: EmployeeSearchComponent },
+  { path: '**', component: NotFoundComponent },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
